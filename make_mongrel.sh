6875dadf5fed93c5170c53b31cdf9f32d1911f0c
hammingweight@gmail.com <hammingweight@gmail.com@ebb761b5-0624-af2b-8f8b-846d3fe60e2b>
2012-08-28 18:21:22 +0000

    Neaten up make script.

--- make_mongrel.sh
+++ make_mongrel.sh
@@ -2,29 +2,47 @@
 VERSION=1.1.7
 BUILD_NAME=mongrel-$VERSION
 set -x
+
+# If the archive already exists, delete it
+if [ -f $BUILD_NAME.zip ]; then
+    rm $BUILD_NAME.zip
+fi
+
+# Create directories for the build artifacts
 mkdir $BUILD_NAME
 mkdir $BUILD_NAME/doc
 mkdir $BUILD_NAME/ebin
 mkdir $BUILD_NAME/tbin
+
+# Compile the source and the tests to separate binary directories
 erlc -I include -o $BUILD_NAME/ebin src/*.erl
 cp src/*.app $BUILD_NAME/ebin
 erlc -I include -o $BUILD_NAME/tbin test/*.erl
+
+# Run all the tests
 erl -noshell -pa $BUILD_NAME/ebin -pa $BUILD_NAME/tbin -s test_all test $BUILD_NAME/tbin -s init stop
+
+# We don't need to keep the test binaries
 rm -r $BUILD_NAME/tbin
+
+# Create the EDocs
 cp overview.edoc $BUILD_NAME/doc
 ./gen_doc.sh $BUILD_NAME/doc
+
+# Copy source files into the artifacts directory
 rsync -p -r --exclude=".*" src $BUILD_NAME
 rsync -p -r --exclude=".*" test $BUILD_NAME
 rsync -p -r --exclude=".*" include $BUILD_NAME
 rsync -p -r --exclude=".*" src_examples $BUILD_NAME
 cp make_mongrel.sh $BUILD_NAME
 cp gen_doc.sh $BUILD_NAME
+
+# Remove some cruft
 rm $BUILD_NAME/doc/overview.edoc
 rm $BUILD_NAME/doc/edoc-info
 cp overview.edoc $BUILD_NAME
-if [ -f $BUILD_NAME.zip ]; then
-    rm $BUILD_NAME.zip
-fi
+
+# Zip it all up.
 zip -q $BUILD_NAME.zip -r $BUILD_NAME
 rm -r $BUILD_NAME
 exit 0
